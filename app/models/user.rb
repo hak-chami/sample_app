c80e9cd0a8be02f8b0aad97f1d7813961e93659c
EC2 Default User <ec2-user@ip-172-31-44-35.ap-southeast-1.compute.internal>
2018-04-15 10:54:03 +0000

    Finish user follow / unfollow actions

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,9 +2,11 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   has_many :microposts, dependent: :destroy
+  has_many :relationships, foreign_key: "follower_id", dependent: :destroy
   has_many :reverse_relationships, foreign_key: "followed_id",
                                    class_name:  "Relationship",
                                    dependent:   :destroy
+  has_many :followers, through: :reverse_relationships, source: :follower
   has_many :followed_users, through: :relationships, source: :followed
   
   def following?(other_user)
